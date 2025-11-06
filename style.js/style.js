d73e78cd629c0876c6ab988bfb0cebf57c82e24b
guigui_bolsonaro <silva.taborda@escola.pr.gov.br>
2025-11-06 12:49:47 +0000

    6

--- style.js/style.js
+++ style.js/style.js
@@ -1,29 +1,21 @@
-const estilos = StyleSheet.create({
-  cartao: {
-    backgroundColor: '#fff',
-    padding: 16,
-    marginTop: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 5,
-    elevation: 5,
-  },
-  foto: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    marginBottom: 10,
-  },
-  nome: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  descricao: {
-    fontSize: 14,
-    color: '#555',
-    textAlign: 'center',
-  },
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+
+    cartao: {
+        backgroundColor: '#e1f7f7',
+        padding: 16,
+        marginTop: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 5,
+        elevation: 5,
+    },
+    foto: { width: 80, height: 80, borderRadius: 40, marginBottom: 10 },
+    nome: { fontSize: 18, fontWeight: 'bold' },
+    descricao: { fontSize: 14, color: '#555', textAlign: 'center' },
+
 });
