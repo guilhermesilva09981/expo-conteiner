82ec2f75bda0d1e9747e88c4124d5eccf46b99f5
guigui_bolsonaro <silva.taborda@escola.pr.gov.br>
2025-11-06 12:41:44 +0000

    4

--- App.js
+++ App.js
@@ -1,23 +1,52 @@
-// App.js
-import React from 'react';
-import { ScrollView } from 'react-native';
-import Titulo from './Titulo';
-import Saudacao from './Saudacao';
-import FotoPerfil from './FotoPerfil';
-import CartaoUsuario from './CartaoUsuario';
+import React from "react";
+import { ScrollView } from "react-native";
+import Titulo from "./componentes/titulo";
+import Saudacao from "./componentes/Saudacao";
+import FotoPerfil from './componentes/FotoPerfil';
+import CartaoUsuario from "./componentes/cartaoUsuario";
+
 
 export default function App() {
-  return (
-    <ScrollView style={{ padding: 20 }}>
-      <Titulo />
-      <Saudacao nome="Schawarski" />
-      <Saudacao nome="Aluna Tech" />
-      <FotoPerfil url="https://randomuser.me/api/portraits/men/1.jpg" />
-      <CartaoUsuario
-        nome="Joana"
-        descricao="Desenvolvedora Mobile em formação"
-        imagem="https://randomuser.me/api/portraits/women/44.jpg"
-      />
-    </ScrollView>
-  );
+ return <ScrollView>
+   <Titulo />
+   <Saudacao nome='Nicolas' />
+   <FotoPerfil />
+
+
+   <CartaoUsuario nome='gabriel'
+     descricao={'Programador Mobile'}
+     img='perfil00'
+   />
+
+
+   <CartaoUsuario nome='Pedro'
+     descricao={'Programador Mobile'}
+     img='perfil01'
+   />
+
+
+   <CartaoUsuario nome='Guilherme'
+     descricao={'Programador Mobile'}
+     img='perfil02'
+   />
+
+
+   <CartaoUsuario nome='nicolas'
+     descricao={'Programador Mobile'}
+     img='perfil03'
+   />
+
+
+   <CartaoUsuario nome='thiago'
+     descricao={'Programador Mobile'}
+     img='perfil04'
+   />
+
+
+ </ScrollView>
+
+
 }
+
+
+
